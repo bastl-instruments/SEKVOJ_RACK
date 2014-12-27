4b58a67f0e3cf543b7d598f0a1e8542db0ab2864
Martin Baar <martin.baar@gmail.com>
2014-12-27 01:06:42 +0100

    Fix merge errors

--- SekvojRackSDPreset.cpp
+++ SekvojRackSDPreset.cpp
@@ -13,10 +13,10 @@ SdVolume vol;           // FAT16 or FAT32 volume
 SdFile root;            // volume's root directory
 SdFile file;
 
-SekvojRackSDPreset::SekvojRackSDPreset(){}
+SekvojRackSDPreset::SekvojRackSDPreset(){
 
+}
 char presetFileName[7]="PQ.txt";
-
 void SekvojRackSDPreset::initCard(unsigned char * data){
 
 	if (!card.init()){/*Serial.println("int");*/};//error("card");
@@ -28,26 +28,27 @@ void SekvojRackSDPreset::initCard(unsigned char * data){
 			//Serial.println("errMake");
 		} else {
 			//Serial.println("makingNewFile");
-			for(int j=0;j<64;j++) {
-				for(int i=0;i<290;i++) file.print(data[i]);
-				for(int i=0;i<222;i++) file.print(255);
-			//Serial.print(".");
-			//Serial.println(file.getC());
+			for (int j = 0; j < 64; j++) {
+				for (int i = 0; i < 290; i++) file.print(data[i]);
+				for (int i = 0; i < 222; i++) file.print(255);
+				//Serial.print(".");
+				//Serial.println(file.getC());
 			}
 		}
-	}
+
+	} else if (!file.open(&root, presetFileName, O_RDWR | O_CREAT )) {}
 }
 
 void SekvojRackSDPreset::getPatternData(unsigned char patternIndex, unsigned char * data) {
 	currentPattern=patternIndex;
-	//Serial.println("load:");
 	uint32_t positionInFile=patternIndex*512;
 	file.seekSet(positionInFile);
 	file.read(&data[0],290);
 }
 
 void SekvojRackSDPreset::setPatternData(unsigned char patternIndex, unsigned char * data) {
-	uint32_t positionInFile = patternIndex * 512;
+	uint32_t positionInFile=patternIndex*512;
 	file.seekSet(positionInFile);
-	file.read(&data[0],290);
+	file.write(&data[0],290);
+
 }
