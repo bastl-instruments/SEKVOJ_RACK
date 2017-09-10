b680c488b89386fcfcbcf0f6ace2e8f06101ae42
Martin Baar <martin.baar@gmail.com>
2017-09-10 13:49:01 +0200

    Make trigger length to be 4 possible values

--- sekvojHW.cpp
+++ sekvojHW.cpp
@@ -16,9 +16,10 @@
 sekvojHW hardware;
 
 //In bastl cycles with perspective that one bastl cycle is 1.5ms
-#define TRIGGER_LENGTH_1 7 		// 10 ms
-#define TRIGGER_LENGTH_2 3		// 5 ms
-#define TRIGGER_LENGTH_3 1		// 1 ms
+#define TRIGGER_LENGTH_1 14 	// 20 ms
+#define TRIGGER_LENGTH_2 7 		// 10 ms
+#define TRIGGER_LENGTH_3 3		// 5 ms
+#define TRIGGER_LENGTH_4 1		// 1 ms
 #define UINT16_MAX 65535
 #define MAX_ADDR 131067
 
@@ -51,6 +52,9 @@ void sekvojHW::setTriggerLength(uint8_t  triggerLength) {
 		break;
 		case 2:
 			trigLength = TRIGGER_LENGTH_3;
+		break;
+		case 4:
+			trigLength = TRIGGER_LENGTH_4;
 		break;
 	}
 }
